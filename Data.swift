2a81c4f7240763c98323723aee991f4487b4258e
Leandro <leanlr96@gmail.com>
2017-07-11 16:34:49 +0100

    Implementation of Data Class (Singleton)

--- Data.swift
+++ Data.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2017 Leandro. All rights reserved.
 //
 
-import Foundation
+class Data {
+    
+    static var shareDate: Data = Data()
+    var fields : [Field]
+    
+    init(){
+        return self.sharedDate
+    }
+    
+    func getFields(of sport: SportEnum) -> Array<Field>{
+        var result : Array<Field> = []
+        for sport in self.fields{
+            if sport.sport == sport{
+                result.append(sport)
+            }
+        }
+        return result
+    }
+    
+    func addField(_ field: Field){
+        self.fields += field
+    }
+    
+    func removeField(_ field: Field) -> Field{
+        for data in self.fields{
+            if data == field{
+                if let index = self.fields.index(of: data){
+                    self.fields.remove(at: index)
+                }
+            }
+        }
+    }
+}
